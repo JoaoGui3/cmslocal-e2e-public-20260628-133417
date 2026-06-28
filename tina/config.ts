f39e90f68c0a31a69603564c46dd98809961d273
CmsLocal E2E <cmslocal@example.test>
2026-06-28 13:34:49 -0300

    Initial E2E fixture

---
+++ tina/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: "main",
+  clientId: null,
+  token: null,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+        ],
+      },
+    ],
+  },
+});
